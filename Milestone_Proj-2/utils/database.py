10601077024f846000c761dde67faf62cb9dd45f
RajithaKelath <102799695+RajithaKelath@users.noreply.github.com>
2023-06-14 11:55:11 +0530

    Add files via upload
    added Type hinting

--- Milestone_Proj-2/utils/database.py
+++ Milestone_Proj-2/utils/database.py
@@ -1,42 +1,43 @@
 import sqlite3
-from .database_connection import *
+from typing import Union
 
-def create_book_table():
+from utils.database_connection import DatabaseConnection
+
+def create_book_table() -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
 
         cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
 
 
-def db_add_book(name,author):
+def db_add_book(name : str,author : str) -> None: 
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
         try :
             cursor.execute('INSERT INTO books VALUES(?,?,0)' , (name,author))
-        except sqlite3.IntegrityError: 
+        except sqlite3.IntegrityError:
             print ('Book already exist!')
-        except:
-            print("chumma")
-      
 
-
-def db_list_books():
+def db_list_books() -> list[dict[str, Union(str,int)]]:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
 
         cursor.execute('SELECT * FROM books')
 
-        books = [{'name': book[0] , 'author': book[1] , 'read': book[2] } for book in cursor.fetchall()]
+        books = [{'name': book[0] ,
+                  'author': book[1] ,
+                  'read': book[2]} 
+                  for book in cursor.fetchall()]
 
     return books
             
-def db_read_book(name):
+def db_read_book(name) -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
 
         cursor.execute('UPDATE books SET read=1 WHERE name = ?',(name,))
 
-def db_delete_book(name):
+def db_delete_book(name) -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
 
